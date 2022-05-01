fd86eea876f8392a96d33bbb3af417736703eeb4
Davi Restani <davirestanifaria@gmail.com>
2022-05-01 15:59:34 -0300

    Removed camera

--- src/routes.js
+++ src/routes.js
@@ -6,20 +6,18 @@ import Menu from "./Pages/Menu"
 import Article from "./Pages/Article"
 import Account from "./Pages/Account"
 import Sightings from "./Pages/Sightings" 
-import Camera from "./Pages/Camera"
 
 const rootElement = document.getElementById("root");
-export default function () {
+export default function  () {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/"component={Login} />
+        <Route exact path="/" component={Login} />
         <Route exact path="/menu" component={Menu} />
         <Route exact path="/menu/emf" component={Emf} />
         <Route exact path="/menu/article" component={Article} />
         <Route exact path="/menu/account" component={Account} />
         <Route exact path="/menu/sightings" component={Sightings} />
-        <Route exact path="/menu/camera" component={Camera} />
       </Switch>
     </BrowserRouter>
   );
